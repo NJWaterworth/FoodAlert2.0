6c547cb42e92ef88bdb8da2b806e6c75b971892e
Jkharrison <jasperharison@gmail.com>
2020-04-16 16:59:34 -0500

    Removed comments from source code

--- pages/RegisterPage.js
+++ pages/RegisterPage.js
@@ -111,22 +111,6 @@ export default class Register extends React.Component {
         );
     }
 }
-// const styles = StyleSheet.create({
-//     container: {
-//       flex: 1,
-//       alignItems: 'center',
-//       justifyContent: 'center',
-//       backgroundColor: '#ecf0f1', 
-//     },
-//     input: {
-//       width: 200,
-//       height: 44,
-//       padding: 10,
-//       borderWidth: 1,
-//       borderColor: 'black',
-//       marginBottom: 10,
-//     },
-//   });
 const styles = StyleSheet.create({
 	imgBackground:{
 		width: '100%',
